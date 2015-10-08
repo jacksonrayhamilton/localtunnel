b76b5c3d5b6a1f422fa65670a3020dc5c071fcda
Luke Horvat <lukehorvat@gmail.com>
2015-10-09 01:46:45 +1100

    Add start/stop/running methods to Client class

--- lib/localtunnel/client.rb
+++ lib/localtunnel/client.rb
@@ -1,5 +1,23 @@
 module Localtunnel
   class Client
+    @@pid = nil
+
+    def self.start
+      ensure_package
+
+      @@pid = Process.spawn("lt --port 3000") unless running?
+    end
+
+    def self.stop
+      Process.kill("KILL", @@pid) if running?
+    end
+
+    def self.running?
+      !@@pid.nil? && Process.waitpid(@@pid, Process::WNOHANG).nil?
+    rescue Errno::ECHILD
+      false
+    end
+
     def self.ensure_package
       `lt --version`
     rescue Errno::ENOENT
