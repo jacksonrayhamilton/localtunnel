8a856fc3a51a92ea95f6d392ea134f563e601b8f
Luke Horvat <lukehorvat@gmail.com>
2015-10-09 16:52:18 +1100

    Handle client connection errors

--- lib/localtunnel/client.rb
+++ lib/localtunnel/client.rb
@@ -2,21 +2,37 @@ require "tempfile"
 
 module Localtunnel
   class Client
-    @@log = nil
     @@pid = nil
+    @@url = nil
 
     def self.start(options = {})
       raise ClientAlreadyStartedError if running?
       raise NpmPackageNotFoundError unless package_installed?
 
+      # Spawn the localtunnel process.
+      log = Tempfile.new("localtunnel")
       execution_string = "lt"
       execution_string << " -p '#{options[:port]}'" if options.key?(:port)
       execution_string << " -s '#{options[:subdomain]}'" if options.key?(:subdomain)
       execution_string << " -h '#{options[:remote_host]}'" if options.key?(:remote_host)
       execution_string << " -l '#{options[:local_host]}'" if options.key?(:local_host)
-      execution_string << " 1>#{(@@log = Tempfile.new("localtunnel")).path} 2>&1"
-
+      execution_string << " 1>#{log.path} 2>&1"
       @@pid = Process.spawn(execution_string)
+
+      # Get the localtunnel URL.
+      10.times do
+        raise ClientConnectionFailedError unless running?
+
+        log.rewind
+        if match_data = log.read.match(/^your url is: (.*)$/)
+          @@url = !match_data.nil? && match_data.captures[0]
+          break
+        end
+
+        sleep 1
+      end
+
+      raise ClientConnectionFailedError unless @@url
     end
 
     def self.stop
@@ -34,8 +50,7 @@ module Localtunnel
     def self.url
       raise ClientAlreadyStoppedError unless running?
 
-      @@log.rewind
-      @@log.read.match(/^your url is: (.*)$/).captures[0]
+      @@url
     end
 
     def self.package_installed?
@@ -52,5 +67,8 @@ module Localtunnel
   end
 
   class ClientAlreadyStoppedError < StandardError
+  end
+
+  class ClientConnectionFailedError < StandardError
   end
 end
